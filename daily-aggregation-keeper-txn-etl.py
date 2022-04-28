90b21797ae951311d91825fcabb51720a307cf1c
Matt Martin <matt.martin@hey.com>
2022-04-28 02:25:09 -0700

    added date and gcs write logic

--- daily-aggregation-keeper-txn-etl.py
+++ daily-aggregation-keeper-txn-etl.py
@@ -40,3 +40,30 @@ client = storage.Client()
 bucket = client.get_bucket('entropy-keeper-transactions')
 
 
+# set date to run on a 15 minute lag against the current datetime
+# this ensures that the final 30 minute window (23:30 to 0:00) of a day gets captured
+date = (datetime.now() - timedelta(minutes=45)).date().strftime('%Y-%m-%d')
+print(datetime.now(), 'Starting on {}'.format(date))
+df_daily_agg = pd.DataFrame()
+
+# gather all the blobs from the entropy keeper transactions bucket for a given day
+blobs = bucket.list_blobs(prefix='raw/'+date)
+
+# iterate through the blobs incrementally adding them to the df_daily_agg dataframe
+for blob in blobs:
+    file_path = 'gs://entropy-keeper-transactions/{}'.format(blob.name)
+
+    df_blob = pd.read_parquet(file_path)
+    
+    df_daily_agg = pd.concat([df_daily_agg, df_blob], axis=0)
+
+df_daily_agg.to_parquet(date+'-daily-aggregation.parquet')
+
+# write the dataframe to google cloud storage as a parquet file
+print(datetime.now(), 'Uploading file to GCS...')
+blob = bucket.blob('daily/'+date+'/'+date+'-daily-aggregation.parquet')
+blob.upload_from_filename(date+'-daily-aggregation.parquet')
+
+# delete the file from local memory
+print(datetime.now(), 'Deleting file from local memory')
+os.remove(date+'-daily-aggregation.parquet')
