ccdf6b9cba70d4f28f803c0cca9feef6db0ba2f8
Matt Martin <matt.martin@hey.com>
2022-05-01 00:54:31 -0700

    added a few more comments

--- total-rewards-as-of.py
+++ total-rewards-as-of.py
@@ -15,17 +15,22 @@ date_str = date.strftime('%Y-%m-%d')
 month_str = date.strftime('%Y-%m')
 
 
+# initialize an empty dataframe to aggregate all daily rewards
 agg_rewards_df = pd.DataFrame()
 
+
+# call list of all blobs in the entropy rewards bucket, daily folder
 blobs = rewards_bucket.list_blobs(prefix='daily/2')
 
 for blob in blobs:
+    print(datetime.now(), "Adding {} to the dataframe...".format(file_path))
     file_path = 'gs://entropy-rewards/{}'.format(blob.name)
     
     temp_df = pd.read_parquet(file_path)
     
     agg_rewards_df = pd.concat([agg_rewards_df,temp_df])
 
+print(datetime.now(), "Aggregating rewards data into the cumulative dataframe...")
 cumulative_rewards = agg_rewards_df.groupby('entropy_keeper_address').sum().sort_values(by='rewards',ascending=False)
 cumulative_rewards.to_parquet('total-rewards-as-of-'+date_str)
 
