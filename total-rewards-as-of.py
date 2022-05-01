f3ddb19396f053f5b2d4440bf050f4f473a8600e
Matt Martin <matt.martin@hey.com>
2022-05-01 00:46:59 -0700

    initial commit

---
+++ total-rewards-as-of.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from google.cloud import storage
+from datetime import datetime, date, timedelta
+import os
+
+
+# initialize google cloud storage client and associated objects
+client = storage.Client()
+rewards_bucket = client.get_bucket('entropy-rewards')
+
+
+# initialize date variables
+date = date.today() - timedelta(days=1)
+date_str = date.strftime('%Y-%m-%d')
+month_str = date.strftime('%Y-%m')
+
+
+agg_rewards_df = pd.DataFrame()
+
+blobs = rewards_bucket.list_blobs(prefix='daily/2')
+
+for blob in blobs:
+    file_path = 'gs://entropy-rewards/{}'.format(blob.name)
+    
+    temp_df = pd.read_parquet(file_path)
+    
+    agg_rewards_df = pd.concat([agg_rewards_df,temp_df])
+
+cumulative_rewards = agg_rewards_df.groupby('entropy_keeper_address').sum().sort_values(by='rewards',ascending=False)
+cumulative_rewards.to_parquet('total-rewards-as-of-'+date_str)
+
+# upload the file to google cloud storage
+print(datetime.now(), 'Uploading file to GCS...')
+blob = rewards_bucket.blob('cumulative/'+month_str+'/total-rewards-as-of-'+date_str)
+blob.upload_from_filename('total-rewards-as-of-'+date_str)
+
+# delete the file from local storage
+print(datetime.now(), 'Deleting file from local memory...')
+os.remove('total-rewards-as-of-'+date_str)
