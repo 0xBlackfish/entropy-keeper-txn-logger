2de89c0c0fb2de6eb9bca7240aa241fb9fc17715
Matt Martin <matt.martin@hey.com>
2022-05-02 02:42:29 -0700

    refactord to match daily rewards etl backfill notebook

--- daily-rewards-etl.py
+++ daily-rewards-etl.py
@@ -19,88 +19,37 @@ date_str = date.strftime('%Y-%m-%d')
 month_str = date.strftime('%Y-%m')
 
 
-# set the file path and read-in the keeper transaction data that will be used to calculate daily rewards
-file_path = 'gs://entropy-keeper-transactions/daily/{}/{}-daily-aggregation.parquet'.format(month_str, date_str)
-print(datetime.now(), "Starting the process for the {} txn set...".format(date_str))
-df = pd.read_parquet(file_path)
+# read in txn time lag sql query
+with open ('gbq_txn_time_lag.sql') as query:
+    query_string = query.read()
 
 
-# create a column which converts the iso timestamp to datetime
-print(datetime.now(), "Formatting and organizing the dataframe...")
-df['date_time'] = df['block.timestamp.iso8601'].apply(lambda x: datetime.strptime(x, '%Y-%m-%dT%H:%M:%SZ'))
+# kick-off the process for writing the previous day's date to gbq and gcs
+print(datetime.now(), "Starting the process for the {} txn set...".format(date))
+
+rewards_df = pd.read_gbq(query=query_string.format(date_str))
+
+rewards_df['daily_total_reward'] = rewards_df['instruction_type'].apply(
+    lambda x: rewards_rate_table.loc[date]['consume_events_reward'] if x == 'ConsumeEvents' else rewards_rate_table.loc[date]['other_events_reward']
+)
+
+rewards_df['daily_keeper_reward'] = rewards_df['pct_of_time'] * rewards_df['daily_total_reward']
+rewards_df['date'] = date_str
 
 
-# sort the dataframe by instruction type and datetime
-df_sorted = df.sort_values(by=['instruction_type','date_time'],ascending=[True, True])
+# create a local parquet file
+print(datetime.now(), 'Creating parquet file...')   
+rewards_df.to_parquet(date_str+'-daily-rewards.parquet')
 
-
-# create a column which shows the previous txns datetime relative to the current txn
-df_sorted['prev_date_time'] = df_sorted.groupby('instruction_type')['date_time'].shift()
-
-
-# calculate the time since last txn
-df_sorted['time_since_last_txn'] = df_sorted['date_time'] - df_sorted['prev_date_time']
-
-
-# decompose the time since last txn into time components, convert them to seconds, and add them together
-time_components = df_sorted['time_since_last_txn'].dt.components
-df_sorted['seconds_since_last_txn'] = (time_components['hours']*3600) + (time_components['minutes']*60) + (time_components['seconds'])
-df_sorted['seconds_since_last_txn'].fillna(0,inplace=True)
-
-
-# calculate the total time for each entropy keeper address
-print(datetime.now(), 'Calculating the total time for each entropy keeper address...')
-consumeEvents_time_dict = {}
-consumeEvents_time = 0
-otherEvents_time_dict = {}
-otherEvents_time = 0
-
-
-for index, txn in df_sorted.iterrows():
-    wallet = txn['entropy_keeper_address']
-
-    if txn['instruction_type'] == 'ConsumeEvents':
-        if wallet not in consumeEvents_time_dict.keys():
-            consumeEvents_time_dict[wallet] = 0
-
-        seconds_since_last_txn = txn['seconds_since_last_txn']
-        consumeEvents_time_dict[wallet] += seconds_since_last_txn
-        consumeEvents_time += seconds_since_last_txn
-
-    else:
-        if wallet not in otherEvents_time_dict.keys():
-            otherEvents_time_dict[wallet] = 0
-
-        seconds_since_last_txn = txn['seconds_since_last_txn']
-        otherEvents_time_dict[wallet] += seconds_since_last_txn
-        otherEvents_time += seconds_since_last_txn
-
-
-# calculat the rewards for each entropy keeper address
-print(datetime.now(), 'Calculating the rewards for each entropy keeper address...')
-consumeEvents_rewards_dict = {}
-otherEvents_rewards_dict = {}
-
-for wallet in consumeEvents_time_dict.keys():
-    consumeEvents_rewards_dict[wallet] = rewards_rate_table['consume_events_reward'][date_str] * (consumeEvents_time_dict[wallet] / consumeEvents_time)
-
-for wallet in otherEvents_time_dict.keys():
-    otherEvents_rewards_dict[wallet] = rewards_rate_table['other_events_reward'][date_str] * (otherEvents_time_dict[wallet] / otherEvents_time)
-
-otherEvents_rewards_df = pd.DataFrame(list(otherEvents_rewards_dict.items()),columns=['entropy_keeper_address','rewards'])
-consumeEvents_rewards_df = pd.DataFrame(list(consumeEvents_rewards_dict.items()),columns=['entropy_keeper_address','rewards'])
-total_rewards_df = pd.concat([otherEvents_rewards_df,consumeEvents_rewards_df]).groupby('entropy_keeper_address').sum().sort_values(by='rewards',ascending=False)
-total_rewards_df['date'] = date
-
-
-# write the total reward dataframe into local memory
-total_rewards_df.to_parquet(date_str+'-daily-rewards.parquet')
+# write the file to google big query
+print(datetime.now(), 'Writing data to GBQ...')
+rewards_df.to_gbq('entropy.keeper_rewards_daily',if_exists='append')
 
 # upload the file to google cloud storage
 print(datetime.now(), 'Uploading file to GCS...')
-blob = rewards_bucket.blob('daily/2022-04/'+date_str+'-daily-rewards.parquet')
-blob.upload_from_filename(date_str+'-daily-rewards.parquet')
+blob = rewards_bucket.blob('daily/'+month_str+'/'+date_str+'-daily-rewards.parquet')
+blob.upload_from_filename(date+'-daily-rewards.parquet')
 
 # delete the file from local storage
 print(datetime.now(), 'Deleting file from local memory...')
-os.remove(date_str+'-daily-rewards.parquet')
+os.remove(date+'-daily-rewards.parquet')
