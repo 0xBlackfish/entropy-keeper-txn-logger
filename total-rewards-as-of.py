3dd4bd8d2edc25f359aea1ea34bc6bec6cdc827e
Matt Martin <matt.martin@hey.com>
2022-05-02 20:05:10 -0600

    refactored to bring in line with notebook backfill

--- total-rewards-as-of.py
+++ total-rewards-as-of.py
@@ -15,30 +15,35 @@ date_str = date.strftime('%Y-%m-%d')
 month_str = date.strftime('%Y-%m')
 
 
-# initialize an empty dataframe to aggregate all daily rewards
-agg_rewards_df = pd.DataFrame()
+# read in txn time lag sql query
+with open ('gbq_total_rewards.sql') as query:
+    query_string = query.read()
 
 
-# call list of all blobs in the entropy rewards bucket, daily folder
-blobs = rewards_bucket.list_blobs(prefix='daily/2')
+# kick-off the process for writing the previous day's date to gbq and gcs
+print(datetime.now(), "Starting the process for the {} txn set...".format(date))
 
-for blob in blobs:
-    print(datetime.now(), "Adding {} to the dataframe...".format(file_path))
-    file_path = 'gs://entropy-rewards/{}'.format(blob.name)
-    
-    temp_df = pd.read_parquet(file_path)
-    
-    agg_rewards_df = pd.concat([agg_rewards_df,temp_df])
+rewards_df = pd.read_gbq(query=query_string.format(date_str))
+rewards_df['date'] = date_str
 
-print(datetime.now(), "Aggregating rewards data into the cumulative dataframe...")
-cumulative_rewards = agg_rewards_df.groupby('entropy_keeper_address').sum().sort_values(by='rewards',ascending=False)
-cumulative_rewards.to_parquet('total-rewards-as-of-'+date_str)
+
+# create a local parquet file
+print(datetime.now(), 'Creating parquet file...')   
+rewards_df[['as_of_date','entropy_keeper_address','total_keeper_reward']].to_parquet('total-rewards-as-of-'+date+'.parquet')
+
+# write the file to google bigquery
+print(datetime.now(), 'Writing data to GBQ...')
+rewards_df[['as_of_date','entropy_keeper_address','total_keeper_reward']].to_gbq('entropy.keeper_rewards_total',if_exists='append')
 
 # upload the file to google cloud storage
 print(datetime.now(), 'Uploading file to GCS...')
-blob = rewards_bucket.blob('cumulative/'+month_str+'/total-rewards-as-of-'+date_str)
-blob.upload_from_filename('total-rewards-as-of-'+date_str)
+blob = rewards_bucket.blob('cumulative/'+date[:7]+'/total-rewards-as-of-'+date+'.parquet')
+blob.upload_from_filename('total-rewards-as-of-'+date+'.parquet')
+
+print(datetime.now(), 'Uploading "current" file to GCS...')
+blob = rewards_bucket.blob('cumulative/current-total-rewards.parquet')
+blob.upload_from_filename('total-rewards-as-of-'+date+'.parquet')
 
 # delete the file from local storage
 print(datetime.now(), 'Deleting file from local memory...')
-os.remove('total-rewards-as-of-'+date_str)
+os.remove('total-rewards-as-of-'+date+'.parquet')
